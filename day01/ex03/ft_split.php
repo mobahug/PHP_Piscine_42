e33a82055dc7e46050b89ed39ed197940d757684
Gabor Horvath-Ulenius <mobahug@gmail.com>
2022-03-23 15:50:43 +0200

    for today i am done

--- day01/ex03/ft_split.php
+++ day01/ex03/ft_split.php
@@ -1,15 +1,8 @@
 <?php
 	function ft_split($str)
 	{
-		$hamburger = array_filter(explode(' ', $str));
+		$hamburger = preg_split('/\s+/', $str);
 		sort($hamburger);
 		return $hamburger;
 	}
 ?>
-
-<!--
-	array_filter	-	just looping through the string
-
-	explode			-	specifying the variable to split
-						then using the given variable
--->
--- day01/ex05/epur_str.php
+++ day01/ex05/epur_str.php
@@ -3,7 +3,7 @@
 	if ($argc > 1)
 	{
 		$str = trim($argv[1]);
-		$str = preg_replace('/ +/', ' ', $str);
+		$str = preg_replace('/\s+/', ' ', $str);
 		if ($str)
 		{
 			echo $str . "\n";
@@ -13,4 +13,9 @@
 
 <!--
 	https://stackoverflow.com/questions/13516162/regex-for-single-space
+
+	preq_replace	'/ +/' REGEX code where we specify only one
+							single replacement we want
+					' '		telling from what we want only one
+
 -->
