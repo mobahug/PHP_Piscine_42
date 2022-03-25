9f7353db1717793ab0b6a6408463cdb56e2ada97
Gabor Horvath-Ulenius <ghorvath@student.hive.fi>
2022-03-25 15:54:50 +0200

    done

--- day02/ex01/one_more_time.php
+++ day02/ex01/one_more_time.php
@@ -1,48 +1,52 @@
 #!/usr/bin/php
 <?php
 
+	date_default_timezone_set('Europe/Paris');
 	$days = array(
-		1 => "lundi",
-		2 => "mardi",
-		3 => "mercredi",
-		4 => "jeudi",
-		5 => "vendredi",
-		6 => "samedi",
-		7 => "dimanche"
+		"lundi",
+		"mardi",
+		"mercredi",
+		"jeudi",
+		"vendredi",
+		"samedi",
+		"dimanche"
 	);
 
 	$months = array(
-		1 => "janvier",
-		2 => "février",
-		3 => "mars",
-		4 => "avril",
-		5 => "mai",
-		6 => "juin",
-		7 => "juillet",
-		8 => "aout",
-		9 => "septembre",
-		10 => "octobre",
-		11 => "novembre",
-		12 => "décembre"
+		"Jan" => "janvier",
+		"Feb" => "février",
+		"Mar" => "mars",
+		"Apr" => "avril",
+		"May" => "mai",
+		"Jun" => "juin",
+		"Jul" => "juillet",
+		"Aug" => "aout",
+		"Sep" => "septembre",
+		"Oct" => "octobre",
+		"Nov" => "novembre",
+		"Dec" => "décembre"
 	);
 	if ($argc > 1)
 	{
 		$data = explode(' ', $argv[1]);
 		$timepattern = "/(^0[0-9]$|1[0-9]|2[0-3]):(0[0-9]|[0-5][0-9]):(0[0-9]|[0-5][0-9])$/";
 		$datepattern = "/(^[1-9]$|^1[0-9]$|^2[0-9]$|^3[0-1])$/";
+		$checks = 0;
 		if (count($data) == 5)
 		{
+			$checks += preg_match($datepattern, $data[1], $date);
+			$checks += preg_match($timepattern, $data[4], $time);
 			$day = array_search(lcfirst($data[0]), $days, 1);
-			$date = preg_match($datepattern, $data[1]);
 			$month = array_search(lcfirst($data[2]), $months, 1);
 			$year = intval($data[3]);
-			$time = preg_match($timepattern, $data[4]);
+		}
+		if ($checks === 2 && $day != NULL && $month != NULL && $year >= 1970 && $year <= time())
+		{
+			$str = "$month $date[0] $year $time[0]";
+			$timestamp = strtotime($str);
+			echo $timestamp;
 		}
 		else
-			echo "Wrong format";
+			echo "Wrong Format\n";
 	}
-
-//	$currentTime;
-//	$res = time($currentTime);
-//	echo $res . "\n";
 ?>
