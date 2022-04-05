8c3c8f06ea8a32eb26ca340fe0669662cef83551
Gabor Horvath-ulenius <mobahug@gmail.com>
2022-04-05 09:15:55 +0300

    starting the day

---
+++ day05/ex16/ex16.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(date_last_film) AS 'movies'
+FROM db_ghorvath.member
+WHERE member.date_last_film BETWEEN '2006-10-30' AND '2007-07-27'
+OR (MONTH(date_last_film) = 12 AND DAY(date_last_film) = 24);
